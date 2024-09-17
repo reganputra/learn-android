455b6944d31be1e88df52146eb0a24b72270f469
reganputra <ramadhanregan334@gmail.com>
2024-09-17 07:36:53 +0700

    membuat aplikasi pengenalan genderasi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // intialize component
+        val tgl_lahir = findViewById<EditText>(R.id.tgl_lahir)
+        val btn_click = findViewById<Button>(R.id.btn_click)
+        val tv_result = findViewById<TextView>(R.id.tv_result)
+
+        btn_click.setOnClickListener{
+            val input = tgl_lahir.text.toString()
+            val result = when(input.toInt()){
+                in 1945..1964 -> "You are baby boomer"
+                in 1965..1980 -> "You are X"
+                in 2000..2016 -> "You are Z"
+                else -> "YNTKTS"
+            }
+            tv_result.text = "$result"
         }
     }
 }
